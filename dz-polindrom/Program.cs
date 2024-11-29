b5c7d1ce62afb0296b6b143d1c401c406d8b3f6d
pollosatyi <88507116+pollosatyi@users.noreply.github.com>
2024-11-29 07:40:34 +0300

    Add project files.

---
+++ dz-polindrom/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace dz_polindrom
+{
+    internal class Program
+    {
+        public static bool IsPolindrom(int number)
+        {
+            int duble = number;
+            int reverseNumber = 0;
+            while (duble > 0)
+            {
+                reverseNumber = reverseNumber * 10 + duble % 10;
+                duble = duble / 10;
+
+            }
+            return reverseNumber == number;
+        }
+
+        public static void PrintMessage(bool isPolindrom)
+        {
+            Console.WriteLine(isPolindrom ? "Число палиндром" : "Число не палиндром");
+        }
+
+        static void Main(string[] args)
+        {
+            if (int.TryParse(Console.ReadLine(), out int number))
+            {
+                PrintMessage(IsPolindrom(number));
+            }
+            else
+            {
+                Console.WriteLine("Неверный ввод числа");
+            }
+        }
+    }
+}
